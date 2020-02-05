76403210affa474a47b5f0aca8a77d57886baa8f
XxshiftxX <contact@dev-shift.me>
2020-02-05 10:31:23 +0900

    create playset db n schema

---
+++ src/Model/Playset.ts
@@ -0,0 +1,45 @@
+import * as mongoose from 'mongoose';
+
+export interface Category {
+  title: string;
+  items: string[];
+}
+
+export interface Playset extends mongoose.Document {
+  title: string;
+  relation: Category[];
+  desire: Category[];
+  place: Category[];
+  object: Category[];
+}
+
+const categorySchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  items: { type: [String], validate: (arr: string[]) => arr.length === 6 },
+});
+
+const playsetSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  relation: {
+    type: [categorySchema],
+    required: true,
+    validate: (arr: Category[]) => arr.length === 6,
+  },
+  desire: {
+    type: [categorySchema],
+    required: true,
+    validate: (arr: Category[]) => arr.length === 6,
+  },
+  place: {
+    type: [categorySchema],
+    required: true,
+    validate: (arr: Category[]) => arr.length === 6,
+  },
+  object: {
+    type: [categorySchema],
+    required: true,
+    validate: (arr: Category[]) => arr.length === 6,
+  },
+});
+
+export const PlaysetModel = mongoose.model<Playset>('Playset', playsetSchema);
---
+++ src/db.ts
@@ -0,0 +1,27 @@
+import * as mongoose from 'mongoose';
+import { Playset, PlaysetModel } from './Model/Playset';
+
+export class PlaysetDB {
+  constructor() { }
+
+  create (playset: Playset): Promise<Playset> {
+    const model = new PlaysetModel(playset);
+    return model.save();
+  }
+
+  read (query: any): mongoose.DocumentQuery<Playset[], Playset> {
+    return PlaysetModel.find(query);
+  }
+
+  readOne (query: any): mongoose.DocumentQuery<Playset | null, Playset> {
+    return PlaysetModel.findOne(query);
+  }
+
+  update (playset: Playset): mongoose.Query<number> {
+    return PlaysetModel.update({ _id: playset._id }, { ...playset });
+  }
+
+  delete (playset: Playset) {
+    return PlaysetModel.remove({ _id: playset._id });
+  }
+}
