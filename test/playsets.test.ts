43d330f99f9c3719adbec0b178f07cf1dace9616
XxshiftxX <contact@dev-shift.me>
2020-02-12 16:19:43 +0900

    make test case about "Create Playsets"

--- test/playsets.test.ts
+++ test/playsets.test.ts
@@ -1,7 +1,8 @@
 import * as sinon from 'sinon';
 import { PlaysetDB } from '../src/db';
-import { getPlaysets } from '../src/service';
+import { getPlaysets, postPlaysets } from '../src/service';
 import { should } from 'chai';
+import { generateSampleData } from './sampledata';
 
 should();
 
@@ -31,6 +32,51 @@ describe('Service Test', () => {
       result.should.be.contains({ _id: 'e' });
       result.should.be.contains({ _id: 'f' });
       readStub.called.should.be.true;
+    });
+  });
+
+  describe('Create New Playset', () => {
+    it('Normal case', async () => {
+      const createStub = sinon.stub(PlaysetDB.prototype, 'create');
+      const sample = generateSampleData();
+
+      await postPlaysets(sample);
+
+      createStub.withArgs(sample).called.should.be.true;
+    });
+
+    it('Wrong type as any', async () => {
+      const createStub = sinon.stub(PlaysetDB.prototype, 'create');
+      const sample = {} as any;
+
+      const invoke = () => postPlaysets(sample);
+
+      invoke.should.throws(TypeError);
+      createStub.called.should.be.false;
+    });
+
+    it('Create playset with page not have 6 category', () => {
+      const createStub = sinon.stub(PlaysetDB.prototype, 'create');
+
+      const sample = generateSampleData();
+      sample.desire = sample.desire.slice(0, 6);
+
+      const invoke = () => postPlaysets(sample);
+
+      invoke.should.throws(TypeError);
+      createStub.called.should.be.false;
+    });
+
+    it('Create playset with category not have 6 item', () => {
+      const createStub = sinon.stub(PlaysetDB.prototype, 'create');
+
+      const sample = generateSampleData();
+      sample.desire[0].items = sample.desire[0].items.slice(0, 6);
+
+      const invoke = () => postPlaysets(sample);
+
+      invoke.should.throws(TypeError);
+      createStub.called.should.be.false;
     });
   });
 });
